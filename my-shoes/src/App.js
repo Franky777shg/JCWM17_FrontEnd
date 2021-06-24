b6a6d87856026381fd92156f6f6cab564b453833
Franky777shg <frengky.sihombing.777@gmail.com>
2021-06-24 10:41:26 +0700

    fix bug

--- my-shoes/src/App.js
+++ my-shoes/src/App.js
@@ -50,20 +50,21 @@ class App extends React.Component {
           </Switch>
         </div>
       );
-    } else {
-      <div style={{ backgroundColor: '#A3DDCB' }}>
-          <Switch>
-            <Route path="/" component={HomePage} exact />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/register" component={RegisPage} />
-            <Route path="/detail" component={DetailPage} />
-            <Route path="/cart" component={CartPage} />
-            <Route path="/history" component={HistoryPage} />
-            <Route path="/historyadmin" component={HistoryAdmin} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
     }
+    return (
+      <div style={{ backgroundColor: '#A3DDCB' }}>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisPage} />
+          <Route path="/detail" component={DetailPage} />
+          <Route path="/cart" component={CartPage} />
+          <Route path="/history" component={HistoryPage} />
+          <Route path="/historyadmin" component={HistoryAdmin} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    )
   }
 }
 
